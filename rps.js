aff944092a34fb1f6e768f35b95469d6d9df0c2c
MBanerjea <mayukh.flip22@gmail.com>
2023-01-25 13:52:12 +0530

    add program framework in plain english

---
+++ rps.js
@@ -0,0 +1,17 @@
+// Take user input
+// Check input for Rock Paper or Scissors
+// Accept input id there's a match. Else, prompt "incorrect input" and ask for input again
+// Store input in const variable
+
+// Create logic for computer choice
+// Random selection from (Rock, Paper, Scissors)
+
+// Compare user input with computer choice
+// Compare strings after converting all to lower case.
+// R<P:R>S || P<S:P>R || S<R:S>P
+
+// Score for user or computer: start a variable from 0 and increment by 1 for every win.
+
+// Repeat game 4 more times
+
+// Tally scores and declare winner
